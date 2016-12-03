5f6450db5120ed6f5fde0d9d340e544519953564
isklikas <isklikas@freemail.gr>
2016-12-03 23:48:17 +0200

    Initial Commit

---
+++ sYoutube-dl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sYoutube-dl
+//
+//  Created by Yannis on 03/12/2016.
+//  Copyright © 2016 isklikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sYoutube-dl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sYoutube-dl
+//
+//  Created by Yannis on 03/12/2016.
+//  Copyright © 2016 isklikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
